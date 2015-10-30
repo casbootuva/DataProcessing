2dc49f73ee46bdbb93536565260eb35f31af8a31
Cas Boot <cas.boot@student.uva.nl>
2015-10-30 19:05:00 +0100

    test
    test

---
+++ problemsets/Scraping/exercise.py
@@ -0,0 +1,49 @@
+# Name : Cas Boot
+# Student number : 10276823
+'''
+This module contains an implementation of split_string.
+'''
+
+# You are not allowed to use the standard string.split() function, use of the
+# regular expression module, however, is allowed.
+# To test your implementation use the test-exercise.py script.
+
+# A note about the proper programming style in Python:
+#
+# Python uses indentation to define blocks and thus is sensitive to the
+# whitespace you use. It is convention to use 4 spaces to indent your
+# code. Never, ever mix tabs and spaces - that is a source of bugs and
+# failures in Python programs.
+
+
+def split_string(source, separators):
+
+	split_value = []
+
+	i = 0
+	j = 0
+	while True:
+		if i == len(source):
+			if source[len(source)-1] in separators:
+				split_value.append(source[j:len(source)])
+				break
+			else:
+				split_value.append(source[j:len(source)])
+				break
+		if source[i] in separators:
+			split_value.append(source[j:i])
+			i+=1
+			j=i
+		else:
+			i+=1
+	split_value = filter(None, split_value)
+
+		
+	print split_value
+
+
+
+if __name__ == '__main__':
+    # You can try to run your implementation here, that will not affect the
+    # automated tests.
+    print split_string('aabbaa', 'b')  # should print: ['c', 'd', 'r']
---
+++ problemsets/Scraping/tvscraper.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# Name:
+# Student number:
+'''
+This script scrapes IMDB and outputs a CSV file with highest ranking tv series.
+'''
+# IF YOU WANT TO TEST YOUR ATTEMPT, RUN THE test-tvscraper.py SCRIPT.
+import csv
+
+from pattern.web import URL, DOM
+
+TARGET_URL = "http://www.imdb.com/search/title?num_votes=5000,&sort=user_rating,desc&start=1&title_type=tv_series"
+BACKUP_HTML = 'tvseries.html'
+OUTPUT_CSV = 'tvseries.csv'
+
+
+def extract_tvseries(dom):
+    '''
+    Extract a list of highest ranking TV series from DOM (of IMDB page).
+
+    Each TV series entry should contain the following fields:
+    - TV Title
+    - Ranking
+    - Genres (comma separated if more than one)
+    - Actors/actresses (comma separated if more than one)
+    - Runtime (only a number!)
+    '''
+
+    #print dom.body.content
+    genrestring =""
+    actorstring =""
+    tvlist = []
+    row = []
+
+    for e in dom.by_tag("tr.detailed")[:50]: # tr. detailed contains all information about the show
+        row = []
+        genrestring =""
+        actorstring =""
+        for a in e.by_tag("td.title"): # td title contains title, genre and actors.
+            for j in a.by_tag("a")[:1]: # refers to title.
+                row.append(str(j[0]))
+            for b in e.by_tag("td.number")[:1]: # refers to rank
+                row.append(str(b[0]))
+            for c in a.by_tag("span.genre"): # refers to span that contains genre.
+                for h in c.by_tag("a"):  # refers to entries in genre span.  
+                    genrestring += str(h[0]) # adds the genre to genre list
+                    genrestring += ", " #adds a comma and space in between the genre
+                genrestring = genrestring[:-2] # makes sure last comma and space are deleted
+                row.append(genrestring)
+            for l in a.by_tag("span.credit"): # refers to the span that contains the actors
+                for m in l.by_tag("a"):   # refers to entries within credit span. 
+                    actorstring += str(m[0]) # adds actors to string.
+                    actorstring += ", "   # adds a comma and space in between.
+                actorstring = actorstring[:-2] # chopps off lost comma and space.
+                row.append(actorstring)
+        tvlist.append(row)
+    return tvlist        
+
+    # ADD YOUR CODE HERE TO EXTRACT THE ABOVE INFORMATION ABOUT THE
+    # HIGHEST RANKING TV-SERIES
+    # NOTE: FOR THIS EXERCISE YOU ARE ALLOWED (BUT NOT REQUIRED) TO IGNORE
+    # UNICODE CHARACTERS AND SIMPLY LEAVE THEM OUT OF THE OUTPUT.
+
+
+def save_csv(f, tvlist):
+    '''
+    Output a CSV file containing highest ranking TV-series.
+    '''
+    writer = csv.writer(f)
+    writer.writerow(['Title', 'Ranking', 'Genre', 'Actors', 'Runtime'])
+    for row in tvlist:
+        writer.writerow(row)
+
+    
+
+    # ADD SOME CODE OF YOURSELF HERE TO WRITE THE TV-SERIES TO DISK
+
+if __name__ == '__main__':
+    # Download the HTML file
+    url = URL(TARGET_URL)
+    html = url.download()
+
+    # Save a copy to disk in the current directory, this serves as an backup
+    # of the original HTML, will be used in testing / grading.
+    with open(BACKUP_HTML, 'wb') as f:
+        f.write(html)
+
+    # Parse the HTML file into a DOM representation
+    dom = DOM(html)
+
+    # Extract the tv series (using the function you implemented)
+    tvseries = extract_tvseries(dom)
+
+    # Write the CSV file to disk (including a header)
+    with open(OUTPUT_CSV, 'wb') as output_file:
+        save_csv(output_file, tvseries)
